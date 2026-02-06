fdc3836277cee40ea111a0b1cd2b2f9895fac7a2
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2026-02-06 15:54:00 -0500

    Adding font family into material-theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+
+// Define the base URL for the backend API
+export const API_URL = 'http://localhost:3000';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
