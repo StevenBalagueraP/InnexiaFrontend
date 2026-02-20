3125dc8c9f50bdbf10c83124e044db0ec3927879
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2026-02-20 09:45:39 -0500

    Adding icon color into room component

--- src/app/shared/components/room/room.component.ts
+++ src/app/shared/components/room/room.component.ts
@@ -1,10 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Room } from '../../../core/interfaces/models/room';
 import { RoomSuggestion } from '../../../core/interfaces/models/RoomSuggestion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-room',
   standalone: true,
+  imports: [MatIconModule],
   templateUrl: './room.component.html',
   styleUrl: './room.component.css',
 })
