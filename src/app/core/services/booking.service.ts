bf21096e9020e5c2e390815bc7dd04c1c4f1723d
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2026-03-03 22:17:07 -0500

    Adding router to the user icon into the sidebar when the user click them

--- src/app/core/services/booking.service.ts
+++ src/app/core/services/booking.service.ts
@@ -2,6 +2,57 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface BookingGuests {
+    adults: number;
+    children: number;
+    childrenAges: number[];
+    _id: string;
+}
+
+export interface BookingRoom {
+    _id: string;
+    hotel: string;
+    type: string;
+    price: number;
+    isAvailable: boolean;
+    capacity: number;
+    __v: number;
+}
+
+export interface BookingHotel {
+    _id: string;
+    name: string;
+    location: string;
+    description: string;
+    amenities: string[];
+    images: string[];
+    __v: number;
+}
+
+export interface BookingUser {
+    _id: string;
+    name: string;
+    email: string;
+    __v: number;
+}
+
+export interface Booking {
+    _id: string;
+    user: BookingUser;
+    hotel: BookingHotel;
+    rooms: BookingRoom[];
+    promotions: any[];
+    checkIn: string;
+    checkOut: string;
+    guests: BookingGuests;
+    totalPrice: number;
+    status: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+    cancelledAt?: string;
+}
+
 @Injectable({
     providedIn: 'root'
 })
@@ -12,4 +63,9 @@ export class BookingService {
     getBookings(): Observable<any> {
         return this.http.get<any>(this.apiUrl);
     }
+
+    getUserBookings(): Observable<Booking[]> {
+        return this.http.get<Booking[]>(this.apiUrl);
+    }
 }
+
