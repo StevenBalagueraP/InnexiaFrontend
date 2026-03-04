b0d801c23dbaadedb2e8587b67fc51210c9335c9
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2026-03-04 00:32:06 -0500

    Adding booking component to render a ficticie design but I didn't implement the figma design I just wanted to see the data :)

--- src/app/core/services/booking.service.ts
+++ src/app/core/services/booking.service.ts
@@ -2,55 +2,22 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface BookingGuests {
-    adults: number;
-    children: number;
-    childrenAges: number[];
-    _id: string;
-}
-
 export interface BookingRoom {
-    _id: string;
-    hotel: string;
+    id: string;
     type: string;
     price: number;
-    isAvailable: boolean;
     capacity: number;
-    __v: number;
-}
-
-export interface BookingHotel {
-    _id: string;
-    name: string;
-    location: string;
-    description: string;
-    amenities: string[];
-    images: string[];
-    __v: number;
-}
-
-export interface BookingUser {
-    _id: string;
-    name: string;
-    email: string;
-    __v: number;
 }
 
 export interface Booking {
-    _id: string;
-    user: BookingUser;
-    hotel: BookingHotel;
-    rooms: BookingRoom[];
-    promotions: any[];
+    id: string;
+    hotelName: string;
+    hotelLocation: string;
+    hotelImage: string | null;
+    status: string;
     checkIn: string;
     checkOut: string;
-    guests: BookingGuests;
-    totalPrice: number;
-    status: string;
-    createdAt: string;
-    updatedAt: string;
-    __v: number;
-    cancelledAt?: string;
+    rooms: BookingRoom[];
 }
 
 @Injectable({
